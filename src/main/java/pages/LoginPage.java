717ed96c3adfc052fa7518669476888f7964b734
Ratko Repic <waren.taily@gmail.com>
2023-10-24 12:27:47 +0200

    Added methods for accessing email, password, and login button elements

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -5,8 +5,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class LoginPage extends BasicPage {
+public class LoginPage extends BasicPage{
     public LoginPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
+    }
+    public WebElement getEmailInput () {
+        return driver.findElement(By.id("email"));
+    }
+    public void clearAndTypeEmail (String email) {
+        getEmailInput().clear();
+        getEmailInput().sendKeys(email);
+    }
+    public String getAttributeTypeForEmailInput () {
+        return getEmailInput().getAttribute("type");
+    }
+    public WebElement getPasswordInput () {
+        return driver.findElement(By.id("password"));
+    }
+    public void clearAndTypePassword (String password) {
+        getPasswordInput().clear();
+        getPasswordInput().sendKeys(password);
+    }
+    public String getAttributeTypeForPasswordInput () {
+        return getPasswordInput().getAttribute("type");
+    }
+    public WebElement getLoginButton () {
+        return driver.findElement(By.cssSelector("button[type='submit']"));
+    }
+    public void clickOnLoginButton () {
+        getLoginButton().click();
+    }
+    public void login (String email, String password) {
+        clearAndTypeEmail(email);
+        clearAndTypePassword(password);
+        clickOnLoginButton();
     }
 }
