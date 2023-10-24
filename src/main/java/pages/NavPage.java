56ec48bcf788a1eedfff956df32e47d425361b16
Ratko Repic <waren.taily@gmail.com>
2023-10-24 12:42:03 +0200

    Create NavPage

---
+++ src/main/java/pages/NavPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class NavPage extends BasicPage{
+    public NavPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+}
