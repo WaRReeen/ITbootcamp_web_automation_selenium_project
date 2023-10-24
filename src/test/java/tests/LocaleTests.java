c340641aae9df8a31db9168e45dec1c1280244bc
Ratko Repic <waren.taily@gmail.com>
2023-10-24 14:40:18 +0200

    Create LocaleTests class

---
+++ src/test/java/tests/LocaleTests.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LandingPage;
+import retry.RetryAnalyzer;
+
+public class LocaleTests extends BasicTest{
+    private LandingPage landingPage;
+
+
+}
