2bc3223c04a04af829103d5c05dbb978e8f128f2
Ratko Repic <waren.taily@gmail.com>
2023-10-24 12:54:29 +0200

    Create ProfilePage

---
+++ src/main/java/pages/ProfilePage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ProfilePage extends BasicPage{
+    public ProfilePage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+
+}
