3619f8a8b015d02d46e26c3baed7d0ae170be036
Ratko Repic <waren.taily@gmail.com>
2023-10-24 14:52:13 +0200

    Create SingUpTest class

---
+++ src/test/java/tests/SignUpTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import retry.RetryAnalyzer;
+
+public class SignUpTest extends BasicTest{
+
+}
