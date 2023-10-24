092dc670eecfeac1500a87ca186cbb6871f7f402
Ratko Repic <waren.taily@gmail.com>
2023-10-24 14:55:07 +0200

    Add Test Methods:visitTheSignupPage,checksInputTypes,displaysErrorsWhenUserAlreadyExists and signup.

--- src/test/java/tests/SignUpTest.java
+++ src/test/java/tests/SignUpTest.java
@@ -6,5 +6,70 @@ import org.testng.annotations.Test;
 import retry.RetryAnalyzer;
 
 public class SignUpTest extends BasicTest{
+    @Test (priority = 1, retryAnalyzer = RetryAnalyzer.class)
+    public void visitsTheSignupPage () {
+        navPage.clickOnTheSignUpButton();
+        Assert.assertEquals(driver.getCurrentUrl(),
+                baseUrl + "/signup",
+                "Current URL should contain '/signup'");
+    }
+    @Test (priority = 2, retryAnalyzer = RetryAnalyzer.class)
+    public void checksInputTypes () {
+        navPage.clickOnTheSignUpButton();
+        Assert.assertEquals(signupPage.getAttributeTypeForEmailInput(),
+                "email",
+                "Attribute type for the email input should be 'email'.");
+        Assert.assertEquals(signupPage.getAttributeTypeForPasswordInput(),
+                "password",
+                "Attribute type for the password input should be 'password'.");
+        Assert.assertEquals(signupPage.getAttributeTypeForConfirmPasswordInput(),
+                "password",
+                "Attribute type for the confirm password input should be 'password'.");
+    }
+    @Test (priority = 3, retryAnalyzer = RetryAnalyzer.class)
+    public void displaysErrorsWhenUserAlreadyExists () {
+        String name = "Another User";
+        String email = "admin@admin.com";
+        String password = "12345";
+        String confirmPassword = "12345";
 
+        navPage.clickOnTheSignUpButton();
+        Assert.assertEquals(driver.getCurrentUrl(),
+                baseUrl + "/signup",
+                "Current URL should contain '/signup'");
+        signupPage.clearAndTypeNameInput(name);
+        signupPage.clearAndTypeEmailInput(email);
+        signupPage.clearAndTypePasswordInput(password);
+        signupPage.clearAndTypeConfirmPasswordInput(confirmPassword);
+        signupPage.clickOnTheSingMeUpButton();
+        messagePopUpPage.waitForThePopUpToBeVisible();
+        Assert.assertEquals(messagePopUpPage.getTheTextFromPopUpMessage(),
+                "E-mail already exists",
+                "Pop up message when user alredy exist should be 'E-mail already exists'.");
+        Assert.assertEquals(driver.getCurrentUrl(),
+                baseUrl + "/signup",
+                "Current URL should contain 'signup'");
+    }
+    @Test (priority = 4, retryAnalyzer = RetryAnalyzer.class)
+    public void signup () {
+        String name = "Djordje Djordjevic";
+        String email = "dj.djordjevic@itbootcamp.rs";
+        String password = "12345";
+        String confirmPassword = "12345";
+
+        navPage.clickOnTheSignUpButton();
+        signupPage.clearAndTypeNameInput(name);
+        signupPage.clearAndTypeEmailInput(email);
+        signupPage.clearAndTypePasswordInput(password);
+        signupPage.clearAndTypeConfirmPasswordInput(confirmPassword);
+        signupPage.clickOnTheSingMeUpButton();
+        wait    .withMessage("User should be redirected to home page.")
+                .until(ExpectedConditions.urlToBe(baseUrl + "/home"));
+        messagePopUpPage.waitForThePopUpMessageWhenUserSignupToBeVisible();
+        Assert.assertEquals(messagePopUpPage.getTheTextFromPopUpMessage(),
+                "IMPORTANT: Verify your account",
+                "Pop up message when user signup should be 'IMPORTANT: Verify your account'");
+        messagePopUpPage.clickOnTheCloseButtonFromPopUpMessage();
+        navPage.clickOnTheLogOutButton();
+    }
 }
