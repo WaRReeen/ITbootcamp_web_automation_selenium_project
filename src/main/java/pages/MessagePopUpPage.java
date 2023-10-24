1356e3892e5376cec184b99cbdc69d444c95aaf3
Ratko Repic <waren.taily@gmail.com>
2023-10-24 12:34:10 +0200

    Create MessagePopUpPage

---
+++ src/main/java/pages/MessagePopUpPage.java
@@ -0,0 +1,43 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MessagePopUpPage extends BasicPage {
+    public MessagePopUpPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public void waitForThePopUpToBeVisible() {
+        wait.withMessage("Pop up message for invalid login should be visible.")
+                .until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("div.error div[role='status']"))));
+    }
+
+    public WebElement getThePopUpMessage() {
+        return driver.findElement(By.cssSelector("div.error div[role='status'] ul li"));
+    }
+
+    public String getTheTextFromPopUpMessage() {
+        return getThePopUpMessage().getText();
+    }
+
+    public WebElement getTheCloseButtonFromPopUpMessage() {
+        return driver.findElement(By.cssSelector("button.btnClose"));
+    }
+
+    public void clickOnTheCloseButtonFromPopUpMessage() {
+        getTheCloseButtonFromPopUpMessage().click();
+    }
+
+    public void waitForThePopUpMessageWhenUserSignupToBeVisible() {
+        wait.withMessage("Popup message when user signup should be visible.")
+                .until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("div.v-card"))));
+    }
+
+    public WebElement getPopUpMessageWhenUserSignUp() {
+        return driver.findElement(By.cssSelector("div.v-card div.v-card__title"));
+    }
+}
