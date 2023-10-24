426a4bff6689e7a260e99c1becb5535ee63b1c4a
Ratko Repic <waren.taily@gmail.com>
2023-10-24 14:35:27 +0200

    Added 4 test methods

--- src/test/java/tests/AuthRoutesTests.java
+++ src/test/java/tests/AuthRoutesTests.java
@@ -5,5 +5,32 @@ import org.testng.annotations.Test;
 import retry.RetryAnalyzer;
 
 public class AuthRoutesTests extends BasicTest{
-
+    @Test (priority = 1, retryAnalyzer = RetryAnalyzer.class)
+    public void forbidsVisitsToHomeUrlIfNotAuthenticated () {
+        driver.navigate().to(baseUrl + "/home");
+        Assert.assertEquals(driver.getCurrentUrl(),
+                baseUrl + "/login",
+                "The user should remain on the login page");
+    }
+    @Test (priority = 2, retryAnalyzer = RetryAnalyzer.class)
+    public void forbidsVisitsToProfileUrlIfNotAuthenticated () {
+        driver.navigate().to(baseUrl + "/profile");
+        Assert.assertEquals(driver.getCurrentUrl(),
+                baseUrl + "/login",
+                "The user should remain on the login page");
+    }
+    @Test (priority = 3, retryAnalyzer = RetryAnalyzer.class)
+    public void forbidsVisitsToAdminCitiesUrlIfNotAuthenticated () {
+        driver.navigate().to(baseUrl + "/admin/cities");
+        Assert.assertEquals(driver.getCurrentUrl(),
+                baseUrl + "/login",
+                "The user should remain on the login page");
+    }
+    @Test (priority = 4, retryAnalyzer = RetryAnalyzer.class)
+    public void forbidsVisitsToAdminUsersUrlIfNotAuthenticated () {
+        driver.navigate().to(baseUrl + "/admin/users");
+        Assert.assertEquals(driver.getCurrentUrl(),
+                baseUrl + "/login",
+                "The user should remain on the login page");
+    }
 }
