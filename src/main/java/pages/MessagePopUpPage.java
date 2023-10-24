b88496f7c58681018afacba3aff82da8b6a288fb
Ratko Repic <waren.taily@gmail.com>
2023-10-24 14:18:35 +0200

    Fixed some errors

--- src/main/java/pages/MessagePopUpPage.java
+++ src/main/java/pages/MessagePopUpPage.java
@@ -6,38 +6,44 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class MessagePopUpPage extends BasicPage {
+public class MessagePopUpPage extends BasicPage{
     public MessagePopUpPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
-
     public void waitForThePopUpToBeVisible() {
-        wait.withMessage("Pop up message for invalid login should be visible.")
+        wait    .withMessage("Pop up message for invalid login should be visible.")
                 .until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("div.error div[role='status']"))));
     }
-
-    public WebElement getThePopUpMessage() {
+    public WebElement getThePopUpMessage () {
         return driver.findElement(By.cssSelector("div.error div[role='status'] ul li"));
     }
-
-    public String getTheTextFromPopUpMessage() {
+    public String getTheTextFromPopUpMessage () {
         return getThePopUpMessage().getText();
     }
-
-    public WebElement getTheCloseButtonFromPopUpMessage() {
+    public WebElement getTheCloseButtonFromPopUpMessage () {
         return driver.findElement(By.cssSelector("button.btnClose"));
     }
-
-    public void clickOnTheCloseButtonFromPopUpMessage() {
+    public void clickOnTheCloseButtonFromPopUpMessage () {
         getTheCloseButtonFromPopUpMessage().click();
     }
-
-    public void waitForThePopUpMessageWhenUserSignupToBeVisible() {
-        wait.withMessage("Popup message when user signup should be visible.")
+    public void waitForThePopUpMessageWhenUserSignupToBeVisible () {
+        wait    .withMessage("Popup message when user signup should be visible.")
                 .until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("div.v-card"))));
     }
-
-    public WebElement getPopUpMessageWhenUserSignUp() {
+    public WebElement getPopUpMessageWhenUserSignUp () {
         return driver.findElement(By.cssSelector("div.v-card div.v-card__title"));
+    }
+    public String getTheTextFromPopUpMessageWhenUserSignUp () {
+        return getPopUpMessageWhenUserSignUp().getText();
+    }
+    public WebElement getSuccessPopUpMessage () {
+        return driver.findElement(By.cssSelector("div.success div[role='status']"));
+    }
+    public void waitForSuccessPopUpMessage() {
+        wait    .withMessage("Successfully pop up message should be visible.")
+                .until(ExpectedConditions.visibilityOf(getSuccessPopUpMessage()));
+    }
+    public boolean getTheTextFromSuccessPopMessage(String message) {
+        return getSuccessPopUpMessage().getText().contains(message);
     }
 }
