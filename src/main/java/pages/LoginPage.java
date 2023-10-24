a75900f1f725fa4b40698e037430ae7cc485af11
Ratko Repic <waren.taily@gmail.com>
2023-10-24 12:21:26 +0200

    Add login page

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage extends BasicPage {
+    public LoginPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+}
