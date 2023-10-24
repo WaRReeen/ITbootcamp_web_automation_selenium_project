0b54b85932656927196e8d9b268fe34e3f32bb1e
Ratko Repic <waren.taily@gmail.com>
2023-10-24 14:43:15 +0200

    Create LoginTests class

---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,9 @@
+package tests;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import retry.RetryAnalyzer;
+
+public class LoginTests extends BasicTest{
+}
